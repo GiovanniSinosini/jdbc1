69e435938bba8e2755ecf0f45f098e5df5ced388
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-29 19:22:27 +0000

    Create database connection

---
+++ src/db/DB.java
@@ -0,0 +1,54 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class DB {
+	
+	
+	private static Connection conn = null;   // variable for connection to the database
+	
+	public static Connection getConnection() {  // method for connection to the database
+		if (conn == null) {   // if there is no connection
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	
+	private static Properties loadProperties() {   // read db.properties
+		
+		try (FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+}
